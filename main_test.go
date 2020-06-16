76f02d733e0097345d6d1cf94ae945bfda3164bf
Sagar Jagtap <sagar.jagtap@scalent.io>
2020-06-16 15:15:53 +0530

    test changed

--- main_test.go
+++ main_test.go
@@ -20,6 +20,14 @@ func Test_adder(t *testing.T) {
 			},
 			want: 3,
 		},
+		{
+			name: "First Positive ",
+			args: args{
+				a: 1,
+				b: -2,
+			},
+			want: -1,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
