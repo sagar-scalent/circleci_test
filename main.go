9a1b0a7407bfcaff507682e99e662e74700d7f5c
Sagar Jagtap <sagar.jagtap@scalent.io>
2020-06-16 15:00:35 +0530

    initial code

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+}
+
+func adder(a, b int) int {
+	return a + b
+}
---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import "testing"
+
+func Test_adder(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "Positive numbers",
+			args: args{
+				a: 1,
+				b: 2,
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := adder(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("adder() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
