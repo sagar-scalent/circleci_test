58ac4265c39f24cc24f0fde026db5ceee5ef07c3
Sagar Jagtap <sagar.jagtap@scalent.io>
2021-06-22 11:09:58 +0530

    added venodr package

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sagar-scalent/circleci_test
+
+go 1.16
+
+require github.com/stretchr/testify v1.7.0
--- main_test.go
+++ main_test.go
@@ -1,6 +1,15 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAddition(t *testing.T) {
+	sum := adder(4, 3)
+	assert.Equal(t, 7, sum, "Sum not the same")
+}
 
 func Test_adder(t *testing.T) {
 	type args struct {
